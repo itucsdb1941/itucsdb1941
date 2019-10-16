1985813ed05a074ef0a6866588dd0e98fa98b2e0
ilgin <balkanilgin@gmail.com>
2019-10-16 12:32:50 +0300

    database added

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
-from flask import Flask
+import sqlite3
+from sqlite3 import Error
+import flask
+from flask import jsonify
+
+app = flask.Flask(__name__)
 
 
-app = Flask(__name__)
+@app.route('/')
+def login():
+	conn = sqlite3.connect("bss.db",  check_same_thread=False)
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM members")
+	data = cursor.fetchall()
+	for item in data:
+		return jsonify(item)
 
-
-@app.route("/")
-def home_page():
-    return "bss is the best"
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '_main_':
+    app.run(port=5000, debug=True)
