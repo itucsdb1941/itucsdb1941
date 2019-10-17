6c6d0caa2d5b0552a109e5b1764761328f623acd
rumeysanurarslan <rumeysanurarslan@gmail.com>
2019-10-17 16:43:51 +0300

    In server.py, get-all-food method added.

--- server.py
+++ server.py
@@ -4,16 +4,23 @@ import flask
 from flask import jsonify
 
 app = flask.Flask(__name__)
+conn = sqlite3.connect("bss.db", check_same_thread=False)
+cursor = conn.cursor()
 
 
 @app.route('/')
 def login():
-	conn = sqlite3.connect("bss.db",  check_same_thread=False)
-	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM personalData")
-	data = cursor.fetchall()
-	for item in data:
-		return jsonify(item)
+    cursor.execute("SELECT * FROM personalData")
+    data = cursor.fetchall()
+    return data
+
+
+@app.route('/foods-list')
+def get_all_food():
+    cursor.execute("SELECT * FROM food")
+    data = cursor.fetchall()
+    return jsonify(data)
+
 
 if __name__ == '_main_':
     app.run(port=5000, debug=True)
