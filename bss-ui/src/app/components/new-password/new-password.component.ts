261bc0b49a2328653f2a9b076789509d0c3de1b2
ilgin <balkanilgin@gmail.com>
2019-10-28 17:37:33 +0300

    new password added

---
+++ bss-ui/src/app/components/new-password/new-password.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Food } from '../../models/app.model';
+import { AppService } from '../../services/app.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-password',
+  templateUrl: './new-password.component.html',
+  styleUrls: ['./new-password.component.css']
+})
+export class NewPasswordComponent implements OnInit {
+  Username: string;
+	Password: string;
+	Answer: string;
+	memberId: any[] = [];
+	members: any[] = [];
+
+  constructor(private route: ActivatedRoute, private appService: AppService, private router: Router) { 
+    this.userLogin();
+	}
+
+	ngOnInit() {
+		
+  }
+  
+  userLogin(): void {
+		this.appService.getMembers().subscribe(member => this.members = member);
+	}
+
+  login(): void{
+		this.members.forEach(row => {
+		
+			this.router.navigateByUrl('/login-page');
+			
+		})
+	}
+	
+}
