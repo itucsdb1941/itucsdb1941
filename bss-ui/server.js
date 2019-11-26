564becd7b443213aeb32a47503ecabb93c49696e
rumeysanurarslan <rumeysanurarslan@gmail.com>
2019-11-26 22:53:59 +0300

    Heroku deploying continued.

--- bss-ui/server.js
+++ bss-ui/server.js
@@ -3,18 +3,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const server = require('http').Server(app);
 
+app.use(express.static(__dirname, 'dist', {index: false}));
+
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/itucsdb1941'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/itucsdb1941/index.html'));
-});
-
-app.get('/login-page', function(req, res) {
-    res.sendFile(path.join(__dirname+ '/src/app/components/login-page/login-page.component.html'));
-});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
