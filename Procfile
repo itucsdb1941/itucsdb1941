aaacf6f47257f44751c8ae121d3af5b9f2e8127c
rumeysanurarslan <rumeysanurarslan@gmail.com>
2019-11-27 16:56:07 +0300

    deleted old project to change the newest

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: npm start
-release: node server.js
--- src/app/models/app.model.ts
+++
@@ -1,8 +0,0 @@
-export class Food {
-    foodID?: number;
-    foodName: string;
-    foodIngre: string;
-    foodRecipe: string;
-    foodPhoto:string;
-    qualificationID  : number;
-  }
